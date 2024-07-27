a050a968c2083225900fda04a7af03ee5f692f28
gozillatiamo <gozilla.tiamo@gmail.com>
2024-07-27 22:18:09 +0700

    add debouncing

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,13 +3,15 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 // @ts-expect-error NextJs no member knowing issue.
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
 
@@ -20,7 +22,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
